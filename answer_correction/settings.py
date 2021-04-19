3881ba6c869b713e8ccff677e87736732bbc6a3d
kk <you@example.com>
2021-04-19 16:50:56 +0530

    Completed Postgres integration

--- answer_correction/settings.py
+++ answer_correction/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')#'rws#(58)p&!qwxqcrw@7r@3-u3rd$-#fn#10zd*763@apkz9v6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["answer-eval.herokuapp.com", '127.0.0.1']
 
