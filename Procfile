7f34957fdcabb986ba3e20275e55734a1d018327
kk <you@example.com>
2021-04-19 23:53:21 +0530

    Completed Postgres integration

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn answer_correction.wsgi --log-file -
+web: gunicorn answer_correction.wsgi --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- answer_correction/settings.py
+++ answer_correction/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import dj_database_url
 import os
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -35,7 +33,7 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',"answer-eval.herokuapp.com"
 # Application definition
 
 INSTALLED_APPS = [
-    #'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
