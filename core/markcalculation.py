001f6bbbf82a8c9dc7fe715697c508bad4a6c4ec
kk <you@example.com>
2021-05-10 00:08:26 +0530

    Added offline feature and upgraded marking algorithm

--- core/markcalculation.py
+++ core/markcalculation.py
@@ -1,5 +1,5 @@
 import os
-import cv2
+import PIL
 from pdf2image import convert_from_path
 import pytesseract
 from nltk.corpus import stopwords
@@ -65,10 +65,14 @@ def handle_uploaded_file(pdf):
         page.save(image_name, "JPEG")
         
         # load the original image
-        img = cv2.imread(image_name)
+        img = PIL.Image(image_name)
 
         # convert the image to black and white for better OCR
-        _,thresh1 = cv2.threshold(img,120,255,cv2.THRESH_BINARY)
+        thresh = 200
+        fn = lambda x : 255 if x > thresh else 0
+        thresh1 = img.convert('L').point(fn, mode='1')
+        
+        #_,thresh1 = cv2.threshold(img,120,255,cv2.THRESH_BINARY)
 
         # pytesseract image to string to get results
         text = str(pytesseract.image_to_string(thresh1, config='--psm 6'))
